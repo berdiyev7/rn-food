952e2d0b844dca6f46029e96f2d99bd0e117224a
Ahmet Berdiyev <berdiyew707@gmail.com>
2022-02-10 13:50:23 +0300

    fix

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import SearchScreen from './src/screens/SearchScreen';
 import { createAppContainer } from 'react-navigation';
