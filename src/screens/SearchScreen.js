0fa4013425743e83d0cab729f485fbe5a2114314
Ahmet Berdiyev <berdiyew707@gmail.com>
2022-02-10 14:37:01 +0300

    styling and adding SearchBox

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import SearchBar from '../components/SearchBar'
 
 const SearchScreen = () => {
    return (
-      <View>
-         <Text>SearchScreen</Text>
+      <View style={styles.searchview}>
+         <SearchBar />
+         <Text>Search Screen</Text>
       </View>
    )
 }
 
-const styles = StyleSheet.create({})
-
+const styles = StyleSheet.create({
+   searchview: {
+      backgroundColor: '#fff'
+   }
+})
 export default SearchScreen
