05f713121332b99bae2b2a39b71e546400df0636
Ahmet Berdiyev <berdiyew707@gmail.com>
2022-02-11 12:54:55 +0300

    add search Api

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,32 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import yelp from '../api/yelp'
 import SearchBar from '../components/SearchBar'
 
 const SearchScreen = () => {
    const [term, setTerm] = useState('')
+   const [result, setResult] = useState([])
+
+   const searchApi = async () => {
+      const response = await yelp.get('/search', {
+         params: {
+            limit: 50,
+            term,
+            location: 'san jose'
+         }
+      });
+      setResult(response.data.businesses);
+   };
 
    return (
       <View style={styles.searchview}>
          <SearchBar
             term={term}
-            onTermChange={newTerm => setTerm(newTerm)}
-            onTermSubmit={() => console.log('term was submitting')}
+            onTermChange={setTerm}
+            onTermSubmit={searchApi}
          />
          <Text>{term}</Text>
+         <Text>We have found {result.length} results</Text>
       </View>
    )
 }
